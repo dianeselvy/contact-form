9e442a3969b40d62d9ccf6a81fb506a44b1e0e61
Diane Selvy <dianeselvy@hotmail.com>
2016-02-26 23:39:19 -0800

    index and form php files
    contact.php is what will make the form functional, and index.php is where you would navigate to find said form

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>Diane Selvy Design</title>
+<!-- favicon info -->
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<!-- end favicon info -->
+<meta name="viewport" content="width=device-width, user-scalable=1">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<style>
+	footer {
+  		position: absolute;
+  		bottom: 0;
+  		width: 100%;
+  		height: 60px;
+		}
+</style>
+
+</head>
+
+<body>
+	<div class="container">
+      <div class="header clearfix" style="background-color: #;">
+        <nav>
+          <ul class="nav nav-pills pull-right" style="padding-top:70px">
+            <li role="presentation"><a href="http://www.dianeselvy.com/gallery/index.html">Gallery</a></li>
+            <li role="presentation"><a href="#">Blog</a></li>
+            <li role="presentation"><a href="http://www.dianeselvy.com/about/index.html">About</a></li>
+            <li role="presentation"><a href="http://www.dianeselvy.com/contact/index.html" class="btn btn-default">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><a href="http://www.dianeselvy.com"><img src="newlogo.png" width="200px" style="padding-bottom:50px;"></a></h3>
+      </div>
+
+<?php include 'contact.php';?>
+      
+
+       
+	</div>
+	<!-- end container -->
+    
+     <footer class="footer">
+      <div class="container">
+        <p class="text-muted"><span class="glyphicon glyphicon-copyright-mark"></span> Diane Selvy 2015, All Rights Reserved.</p>
+      </div>
+    </footer>
+</body>
+</html>
